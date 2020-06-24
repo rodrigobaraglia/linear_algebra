10023e01951b7fbbc7b28911a83e084711b1dc8c
Baraglia <rodrigo.d.baraglia@accenture.com>
2020-06-24 17:34:56 -0300

    comparing less and more verbose implementations

--- cd11576e-85e8-41f3-85f7-716b78d3cd51.hs
+++ cd11576e-85e8-41f3-85f7-716b78d3cd51.hs
@@ -1,10 +1,8 @@
 module Basics where
 
---Sum of vectors (ordered from less to more verbose implementations):
-
+--Sum of vectors:
 sumVec:: Num b => [b] -> [b] -> [b]
 sumVec a b  =  zipWith (+) a b 
-
 sumVec':: Num b => [b] -> [b] -> [b]
 sumVec' a b  = map (\pair -> fst pair + snd pair) $ zip a b 
 
@@ -16,18 +14,18 @@ dotProd a b = sum $ zipWith (*) a b
 dotProd':: Num a => [a] -> [a] -> a
 dotProd' a b = sum $ map (\pair -> fst pair * snd pair) $ zip a b 
 
---Transpose:
+--Transpose
 transpose:: [[a]] -> [[a]]
 transpose ([]:_) = []
 transpose x = (map head x) : transpose (map tail x)
 
---Matrix multiplication (ordered from less to more verbose implementations):
-mMult:: Num c => [[c]] -> [[c]] -> [[c]]
-mMult a b =  zipWith (zipWith (*)) a $ transpose b
+--Matrix multiplication (ordered from more to less verbose implementations)
+mMult'':: Num c => [[c]] -> [[c]] -> [[c]]
+mMult'' a b = map (\pair -> zipWith (*) (fst pair) (snd pair)) ab
+    where ab = zip a $ transpose b
 
 mMult':: Num c => [[c]] -> [[c]] -> [[c]]
 mMult' a b = map (\pair -> zipWith (*) (fst pair) (snd pair)) $ zip a $ transpose b
 
-mMult'':: Num c => [[c]] -> [[c]] -> [[c]]
-mMult'' a b = map (\pair -> zipWith (*) (fst pair) (snd pair)) ab
-    where ab = zip a $ transpose b
+mMult:: Num c => [[c]] -> [[c]] -> [[c]]
+mMult a b =  zipWith (zipWith (*)) a $ transpose b
